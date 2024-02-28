f1149314c65dd83471a13fdc4a8886c6f37ebb9a
K.Umut Araz <150933483+arazumut@users.noreply.github.com>
2024-02-28 13:54:36 +0300

    Add files via upload

---
+++ harfnotu.cpp.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main() {
+    int sinavPuani;
+
+    // Kullanıcıdan sınav puanını alma
+    std::cout << "Sınav puanınızı girin: ";
+    std::cin >> sinavPuani;
+
+    // Puanı harfe çevirme
+    if (sinavPuani >= 90) {
+        std::cout << "Harf notunuz: A" << std::endl;
+    }
+    else if (sinavPuani >= 80) {
+        std::cout << "Harf notunuz: B" << std::endl;
+    }
+    else if (sinavPuani >= 70) {
+        std::cout << "Harf notunuz: C" << std::endl;
+    }
+    else if (sinavPuani >= 60) {
+        std::cout << "Harf notunuz: D" << std::endl;
+    }
+    else {
+        std::cout << "Harf notunuz: F" << std::endl;
+    }
+
+    return 0;
+}
